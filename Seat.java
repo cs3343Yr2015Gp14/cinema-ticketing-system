b9bdd9ad1e2874807ad312172cce499061df6ccf
ConnieIp <conniekamanip@gmail.com>
2015-09-28 18:19:16 +0800

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -4,6 +4,7 @@ public class Seat{
 	boolean allocated;
 	String seat;
 	SeatStatus status;
+	Identity id;
 	
 	public Seat(String aSeat,boolean aAllocated) {
 		seat=aSeat;
@@ -14,4 +15,11 @@ public class Seat{
 		return status;
 	}
 	
+	public void setId(Identity aId){
+		id=aId;
+	}
+
+	public String getSeatName(){
+		return seat;
+	}
 }
