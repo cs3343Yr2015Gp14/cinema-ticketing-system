e0d9446330dfb8a0d062981615643a3a54b38c60
ConnieIp <conniekamanip@gmail.com>
2015-09-28 19:38:42 +0800

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -3,12 +3,14 @@ package cinemaTicket;
 public class Movie {
 	String movie;
 	int code;
+	int price;
 	
 	private String[] allschedule={"11:00","15:00","18:00"};
 	
 	public Movie (int aCode) {
 			movie=MovieOffice.getMovieName(aCode);
 			code=aCode;
+			price=100;
 	}
 	
 	public void getAllSchedule(){
@@ -20,4 +22,13 @@ public class Movie {
 	public int getCode(){
 		return code;
 	}
+	
+	public String getName(){
+		return movie;
+	}
+	
+	public int getPrice(){
+		return price;
+	}
 }
+
