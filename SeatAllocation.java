66c98988e8e34663cf725131daed99566d3b4b0d
ConnieIp <conniekamanip@gmail.com>
2015-09-28 17:14:58 +0800

    Update SeatAllocation.java

--- SeatAllocation.java
+++ SeatAllocation.java
@@ -15,7 +15,7 @@ public class SeatAllocation {
 		if(SeatControllers.get(s)!=null){
 			for(int i=0;i<seats.length;i++)
 				for(int j=0;j<SeatControllers.get(s).length;j++){
-					if((SeatControllers.get(s))[j].toString().equals(seats[i]))
+					if((SeatControllers.get(s))[j].getSeatName().equals(seats[i]))
 						if ((SeatControllers.get(s))[j].getStatus().getStatus().equals("Available"))
 							return false;
 				}
@@ -35,8 +35,15 @@ public class SeatAllocation {
 			SeatControllers.put(aSch,seats);
 		}
 		for(int i=0;i<SeatControllers.get(aSch).length;i++)
-			if((SeatControllers.get(aSch))[i].toString().equals(aSeat.toString()))
+			if((SeatControllers.get(aSch))[i].getSeatName().equals(aSeat.getSeatName()))
 				(SeatControllers.get(aSch))[i]=aSeat;
 	}
-	
+
+	public static Seat getSeat(Schedule aSch,String aSeat){
+		for(int j=0;j<SeatControllers.get(aSch).length;j++){
+			if((SeatControllers.get(aSch))[j].getSeatName().equals(aSeat))
+				return (SeatControllers.get(aSch))[j];
+		}
+		return null;
+	}
 }
